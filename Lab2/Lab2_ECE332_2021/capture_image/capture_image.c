d2d5a208c1940bfbcd829332ad878bdb9872396c
Jongsang Yoo <jamin0513@gmail.com>
2022-07-26 13:55:05 +0900

    그림판 이미지 인터페이싱 성공

--- Lab2/Lab2_ECE332_2021/capture_image/capture_image.c
+++ Lab2/Lab2_ECE332_2021/capture_image/capture_image.c
@@ -9,6 +9,7 @@
 #include "./CNN/cnn_func_integer.h"
 #include "./CNN/ifmap_fmap_float.h"
 #include "./CNN/cnn_func_float.h"
+#include "CNN_img.h"
 
 #define KEY_BASE            0xFF200050
 
@@ -32,6 +33,7 @@ int key_value;
 
 //CNN VAR
 float ofmap_ref[_data][_class] = { { 0, }, };
+float ofmap_img[_class] 	   = { 0, };
 short ofmap_opt[_data][_class] = { { 0, }, };
 
 
@@ -56,17 +58,19 @@ unsigned int initializor_dummy(unsigned int uiParam0, unsigned int uiParam1, uns
 unsigned int validator_dummy(unsigned int uiParam0, unsigned int uiParam1, unsigned int uiParam2, unsigned int uiParam3 );
 void cnn_ref(float *ofmap, float *ifmap, int data_set);
 void cnn_opt(short *ofmap, short *ifmap, int data_set);
+void cnn_img(float *ofmap, float *ifmap);
+
 //void Measure_performance();
 
 int main(void) {
 	while (1){
 		enableVideo();
+		DrawRectangle_filled(50, 50, 28, 28, GREEN);
 		if(*(KEY_ptr) != 0){
 			key_value = *(KEY_ptr);
 			switch (key_value) {
 				case KEY0:		//KEY00
 					printf("Case 0: Reference \r\n");
-					
 					for (data_set = 0; data_set<_data; data_set++){
 						ifmap_f = (float *)calloc(H_C1*W_C1*C_C1*N_C1,sizeof(float));
 						ofmap_f = (float *)calloc(E_C4*F_C4*M_C4*N_C4,sizeof(float));
@@ -143,7 +147,39 @@ int main(void) {
 					break;
 
 				case KEY2:		//KEY02
-					printf("KEY2 \r\n");
+					printf("Case 2: Test IMG FLOAT NUMBER \r\n");
+					ifmap_f = (float *)calloc(H_C1*W_C1*C_C1*N_C1,sizeof(float));
+					ofmap_f = (float *)calloc(E_C4*F_C4*M_C4*N_C4,sizeof(float));
+
+					cnn_img(ofmap_f, ifmap_f);
+
+					/////////////////////////////
+					//     Classifiacation     //
+					////////////////////////////
+					max_val_f = ofmap_f[0];
+					data_index = 0;
+					for (n = 0; n<N_C4; n++) {
+						for (m = 0; m<M_C4; m++) {
+							for (e = 0; e<E_C4; e++) {
+								for (f = 0; f<F_C4; f++) {
+
+									ofmap_img[data_index] = ofmap_f[((n*M_C4 + m)*E_C4 + f)*F_C4 + e];
+
+									if (ofmap_f[((n*M_C4 + m)*E_C4 + f)*F_C4 + e] >= max_val_f) {
+										max_val_f = ofmap_f[((n*M_C4 + m)*E_C4 + f)*F_C4 + e];
+										estimated_label = m;
+									}
+									data_index++;
+								}
+							}
+						}
+					}
+					printf("estimated label: %d (%f) \r\n", estimated_label, max_val_f);
+					free(ifmap_f);
+					free(ofmap_f);
+				
+					printf("DONE\r\n");
+
 					while (*KEY_ptr != 0);	//while the key is pressed down dont do anything
 					break;
 
@@ -194,6 +230,57 @@ void cnn_ref(float *ofmap, float *ifmap, int data_set)
 	//Input mapping
 	for (i = 0; i<H_C1*W_C1; i++) {
 		ifmap[i] = data[data_set][i];
+	}
+
+	/////////////////////////////
+	//         Layer #1       //
+	////////////////////////////
+	convolution_f(ofmap1, ifmap, fmap1_f, N_C1, C_C1, M_C1, F_C1, E_C1, R_C1, S_C1, H_C1, W_C1, U_C1);
+	bias_f(ofmap1, ofmap1, bias1_f, N_C1, M_C1, E_C1, F_C1);
+	pool_f(ofmap2, ofmap1, E_C1, F_C1, M_C1);
+
+	/////////////////////////////
+	//         Layer #2       //
+	////////////////////////////
+	convolution_f(ofmap3, ofmap2, fmap2_f, N_C2, C_C2, M_C2, F_C2, E_C2, R_C2, S_C2, H_C2, W_C2, U_C2);
+	bias_f(ofmap3, ofmap3, bias2_f, N_C2, M_C2, E_C2, F_C2);
+	pool_f(ofmap4, ofmap3, E_C2, F_C2, M_C2);
+
+	/////////////////////////////
+	//         Layer #3       //
+	////////////////////////////
+	convolution_f(ofmap5, ofmap4, fmap3_f, N_C3, C_C3, M_C3, F_C3, E_C3, R_C3, S_C3, H_C3, W_C3, U_C3);
+	bias_f(ofmap5, ofmap5, bias3_f, N_C3, M_C3, E_C3, F_C3);
+	relu_f(ofmap6, ofmap5, E_C3, F_C3, M_C3);
+
+	/////////////////////////////
+	//         Layer #4       //
+	////////////////////////////
+	convolution_f(ofmap, ofmap6, fmap4_f, N_C4, C_C4, M_C4, F_C4, E_C4, R_C4, S_C4, H_C4, W_C4, U_C4);
+	bias_f(ofmap, ofmap, bias4_f, N_C4, M_C4, E_C4, F_C4);
+
+	free(ofmap1);
+	free(ofmap2);
+	free(ofmap3);
+	free(ofmap4);
+	free(ofmap5);
+	free(ofmap6);
+}
+
+void cnn_img(float *ofmap, float *ifmap)
+{
+	int i = 0;
+
+	float *ofmap1 = (float *) calloc(E_C1*F_C1*M_C1*N_C1, sizeof(float));
+	float *ofmap2 = (float *) calloc(E_P1*F_P1*C_P1*N_P1, sizeof(float));
+	float *ofmap3 = (float *) calloc(E_C2*F_C2*M_C2*N_C2, sizeof(float));
+	float *ofmap4 = (float *) calloc(E_P2*F_P2*C_P2*N_P2, sizeof(float));
+	float *ofmap5 = (float *) calloc(E_C3*F_C3*M_C3*N_C3, sizeof(float));
+	float *ofmap6 = (float *) calloc(E_R1*F_R1*M_R1*N_R1, sizeof(float));
+
+	//Input mapping
+	for (i = 0; i<H_C1*W_C1; i++) {
+		ifmap[i] = img_data[i];
 	}
 
 	/////////////////////////////
---
+++ PYformnist/read.py
@@ -0,0 +1,16 @@
+import numpy as np
+from PIL import Image
+
+arr = np.loadtxt("PYformnist/read.txt", delimiter=',')
+arr = ((arr * 255) + 1)
+arr = np.reshape(arr, (28, 28))
+
+arr = np.rot90(arr)
+arr = np.rot90(arr)
+arr = np.rot90(arr)
+arr = np.flip(arr, axis=1) 
+
+im = Image.fromarray(arr)
+im.show()
+
+print(arr)
